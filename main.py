06f9a714ae87a8e998f70c190730bffc609ba432
kolya <kolyan78@gmail.com>
2021-12-18 16:21:46 +0300

    add requirements.txt

--- main.py
+++ main.py
@@ -15,25 +15,27 @@ def get_title(): # Функция-генератор, каждый раз при
     while True:
         yield random.choice(list_)
 
-def get_year(): #
-    return random.randint(1001, 2022)
+def get_year(): # Функция выдает случайный год в заданном диапазоне от 1800 до текущего
+    return random.randint(1800, 2021)
 
-def get_pages():
+def get_pages(): # Функция выдает случайное количество страниц в книге от 10 до 500
     return random.randint(10,500)
 
-def get_isbn13():
+def get_isbn13(): # Функция выдает фейковый ISBN13
     return fake.isbn13()
 
-def get_rating():
+def get_rating(): # Функция выдает случайный рейтинг от 0 до 5 включительно, результат округляется до одного знака после запятой
     return round(random.randint(0, 4) + random.random(), 1)
 
-def get_price():
+def get_price(): # Функция выдает случайную стоимость книги в диапазоне от 10 до 99.99, результат округляется до двух знаков после запятой
     return round(random.randint(10, 99) + random.random(), 2)
 
-def get_author():
+def get_author(): # Функция для генерации фейкового автора, количество авторов выбрается случайным образом от 1 до 3
     return [fake.name() for _ in range(random.randint(1, 3))]
 
-def get_item(pk=1):
+def get_item(pk = 1): # Функция генератор для создания словаря.
+    # Пока не знаю как сделать параметр pk изменяющимся при каждом вызове.
+    # Он всегда выдет одно и то же число
     while True:
         yield {
             "model": conf.MODEL,
@@ -50,25 +52,14 @@ def get_item(pk=1):
         }
         pk += 1
 
+def func(i = 1):
+    while True:
+        yield i
+        i += 1
+
 
 if __name__ == "__main__":
-    list_ = [next(get_item()) for _ in range(5)]
-    print(list_)
+    item = get_item()
+    list_ = [next(item) for _ in range(100)]
     with open("output.json", "w", encoding="utf8") as f:
-
-        f.write(json.dumps(list_))
-        #
-
-
-    # for _ in range(5):
-    #     print(next(get_item()))
-
-    # for _ in range(10):
-    #     print(next(get_title()))
-    #     print(get_year())
-    #     print(get_pages())
-    #     print(get_isbn13())
-    #     print(get_rating())
-    #     print(get_price())
-    #     print(get_author())
-    #     print("---------------------------")
+        f.write(json.dumps(list_, indent = 4, ensure_ascii=False))
